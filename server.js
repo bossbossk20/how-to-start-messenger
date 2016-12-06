5f704167f479fc4ef26ab3ef4ab0ff21183bcdc0
thanawat <tha_chookes@hotmail.com>
2016-12-07 02:20:34 +0700

    added function on webhook

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var request = require('request')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
@@ -19,15 +20,11 @@ app.get('/webhook', function (req, res) {
 })
 
 app.post('/webhook', function (req, res) {
-  var data = req.body
+  let data = req.body
   // Make sure this is a page subscription
   if (data.object === 'page') {
     // Iterate over each entry - there may be multiple if batched
     data.entry.forEach(function (entry) {
-      var pageID = entry.id
-      var timeOfEvent = entry.time
-
-      // Iterate over each messaging event
       entry.messaging.forEach(function (event) {
         if (event.message) {
           receivedMessage(event)
@@ -41,7 +38,70 @@ app.post('/webhook', function (req, res) {
 })
 
 function receivedMessage (event) {
-  console.log('Message data: ', event.message)
+  var senderID = event.sender.id
+  var recipientID = event.recipient.id
+  var timeOfMessage = event.timestamp
+  var message = event.message
+
+  console.log('Received message for user %d and page %d at %d with message:',
+    senderID, recipientID, timeOfMessage)
+  console.log(JSON.stringify(message))
+
+  var messageText = message.text
+  var messageAttachments = message.attachments
+
+  if (messageText) {
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID)
+        break
+
+      default:
+        sendTextMessage(senderID, messageText)
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, 'Message with attachment received')
+  }
+}
+
+function sendGenericMessage (recipientId, messageText) {
+  // To be expanded in later sections
+}
+
+function sendTextMessage (recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  }
+
+  callSendAPI(messageData)
+}
+
+function callSendAPI (messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: 'EAAXEcK9g0skBAE3yIqTZBz6LymGKMZBUIWsCDeLyNbXnDDs2AotvSluqlwa7ZB53DwfFCltZBtIUuouhmuTr5pZAOGTC2n404UbHl3tMs1IRFtPGcnOgMcVDsi61kvXZAqJ9fNibmpeg6NxjZA2DrLyS7nWpmAqnVeajHJ5jdpiEwZDZD' },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var recipientId = body.recipient_id
+      var messageId = body.message_id
+      console.log('Successfully sent generic message with id %s to recipient %s',
+        messageId, recipientId)
+    } else {
+      console.error('Unable to send message.')
+      console.error(response)
+      console.error(error)
+    }
+  })
 }
 
 app.listen(app.get('port'), function () {
