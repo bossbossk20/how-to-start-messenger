835da5d0829a2ac2b82a661a1735fcadd76d7e40
thanawat <tha_chookes@hotmail.com>
2016-12-07 01:41:47 +0700

    app.set port heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 5000))
+
 app.get('/', function (req, res) {
   res.send('hello kkkk')
 })
 
-app.listen(3000, function () {
-  console.log('running on port 3000')
+app.listen(app.get('port'), function () {
+  console.log('running on 5000')
 })
