6c8439598a0990a1682669664a677b01a9894480
thanawat <tha_chookes@hotmail.com>
2016-12-07 01:53:08 +0700

    app webhook

--- server.js
+++ server.js
@@ -7,6 +7,43 @@ app.get('/', function (req, res) {
   res.send('hello kkkk')
 })
 
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'koykoy') {
+    console.log('Validating webhook')
+    res.status(200).send(req.query['hub.challenge'])
+  } else {
+    console.error('Failed validation. Make sure the validation tokens match.')
+    res.sendStatus(403)
+  }
+})
+
+app.post('/webhook', function (req, res) {
+  var data = req.body
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function (entry) {
+      var pageID = entry.id
+      var timeOfEvent = entry.time
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function (event) {
+        if (event.message) {
+          receivedMessage(event)
+        } else {
+          console.log('Webhook received unknown event: ', event)
+        }
+      })
+    })
+    res.sendStatus(200)
+  }
+})
+
+function receivedMessage (event) {
+  console.log('Message data: ', event.message)
+}
+
 app.listen(app.get('port'), function () {
   console.log('running on 5000')
 })
