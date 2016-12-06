32234b7182ae5ee62b50f497980b176c36f96ed7
thanawat <tha_chookes@hotmail.com>
2016-12-07 02:36:14 +0700

    added function on webhook

--- server.js
+++ server.js
@@ -1,110 +1,113 @@
-var express = require('express')
-var request = require('request')
-var app = express()
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
 
 app.set('port', (process.env.PORT || 5000))
-
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.get('/', function (req, res) {
-  res.send('hello kkkk')
+  res.send('test test')
 })
-
-app.get('/webhook', function (req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'koykoy') {
-    console.log('Validating webhook')
-    res.status(200).send(req.query['hub.challenge'])
-  } else {
-    console.error('Failed validation. Make sure the validation tokens match.')
-    res.sendStatus(403)
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'koykoy') {
+    res.send(req.query['hub.challenge'])
   }
+  res.send('Error, wrong token')
 })
-
-app.post('/webhook', function (req, res) {
-  let data = req.body.event
-  // Make sure this is a page subscription
-  // if (data.object === 'page') {
-  //   // Iterate over each entry - there may be multiple if batched
-  //   data.entry.forEach(function (entry) {
-  //     entry.messaging.forEach(function (event) {
-  //       if (event.message) {
-  //         receivedMessage(event)
-  //       } else {
-  //         console.log('Webhook received unknown event: ', event)
-  //       }
-  //     })
-  //   })
-  //   res.sendStatus(200)
-  // }
-  console.log(data)
-})
-
-function receivedMessage (event) {
-  var senderID = event.sender.id
-  var recipientID = event.recipient.id
-  var timeOfMessage = event.timestamp
-  var message = event.message
-
-  console.log('Received message for user %d and page %d at %d with message:',
-    senderID, recipientID, timeOfMessage)
-  console.log(JSON.stringify(message))
-
-  var messageText = message.text
-  var messageAttachments = message.attachments
-
-  if (messageText) {
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID)
-        break
-
-      default:
-        sendTextMessage(senderID, messageText)
+app.post('/webhook/', function (req, res) {
+  let messaging_events = req.body.entry[0].messaging
+  for (let i = 0; i < messaging_events.length; i++) {
+    let event = req.body.entry[0].messaging[i]
+    let sender = event.sender.id
+    if (event.message && event.message.text) {
+      let text = event.message.text
+      if (text === 'Generic') {
+        sendGenericMessage(sender)
+        continue
+      }
+      sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
     }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, 'Message with attachment received')
-  }
-}
-
-function sendGenericMessage (recipientId, messageText) {
-  // To be expanded in later sections
-}
-
-function sendTextMessage (recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
+    if (event.postback) {
+      let text = JSON.stringify(event.postback)
+      sendTextMessage(sender, 'Postback received: ' + text.substring(0, 200), token)
+      continue
     }
   }
+  res.sendStatus(200)
+})
+const token = 'EAAXEcK9g0skBAE3yIqTZBz6LymGKMZBUIWsCDeLyNbXnDDs2AotvSluqlwa7ZB53DwfFCltZBtIUuouhmuTr5pZAOGTC2n404UbHl3tMs1IRFtPGcnOgMcVDsi61kvXZAqJ9fNibmpeg6NxjZA2DrLyS7nWpmAqnVeajHJ5jdpiEwZDZD'
 
-  callSendAPI(messageData)
-}
-
-function callSendAPI (messageData) {
+function sendTextMessage (sender, text) {
+  let messageData = { text: text }
   request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: 'EAAXEcK9g0skBAE3yIqTZBz6LymGKMZBUIWsCDeLyNbXnDDs2AotvSluqlwa7ZB53DwfFCltZBtIUuouhmuTr5pZAOGTC2n404UbHl3tMs1IRFtPGcnOgMcVDsi61kvXZAqJ9fNibmpeg6NxjZA2DrLyS7nWpmAqnVeajHJ5jdpiEwZDZD' },
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
     method: 'POST',
-    json: messageData
-
+    json: {
+      recipient: {id: sender},
+      message: messageData
+    }
   }, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var recipientId = body.recipient_id
-      var messageId = body.message_id
-      console.log('Successfully sent generic message with id %s to recipient %s',
-        messageId, recipientId)
-    } else {
-      console.error('Unable to send message.')
-      console.error(response)
-      console.error(error)
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
+
+function sendGenericMessage (sender) {
+  let messageData = {
+    'attachment': {
+      'type': 'template',
+      'payload': {
+        'template_type': 'generic',
+        'elements': [{
+          'title': 'First card',
+          'subtitle': 'Element #1 of an hscroll',
+          'image_url': 'http://messengerdemo.parseapp.com/img/rift.png',
+          'buttons': [{
+            'type': 'web_url',
+            'url': 'https://www.messenger.com',
+            'title': 'web url'
+          }, {
+            'type': 'postback',
+            'title': 'Postback',
+            'payload': 'Payload for first element in a generic bubble'
+          }]
+        }, {
+          'title': 'Second card',
+          'subtitle': 'Element #2 of an hscroll',
+          'image_url': 'http://messengerdemo.parseapp.com/img/gearvr.png',
+          'buttons': [{
+            'type': 'postback',
+            'title': 'Postback',
+            'payload': 'Payload for second element in a generic bubble'
+          }]
+        }]
+      }
+    }
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: messageData
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
     }
   })
 }
 
 app.listen(app.get('port'), function () {
-  console.log('running on 5000')
+  console.log('running on port', app.get('port'))
 })
