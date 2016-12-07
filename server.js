151d2a6cd57139c37b0bb665e9834d86446ffa37
thanawat <tha_chookes@hotmail.com>
2016-12-07 14:25:24 +0700

    remove token and webhook key

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-const token = 'EAAXEcK9g0skBAE3yIqTZBz6LymGKMZBUIWsCDeLyNbXnDDs2AotvSluqlwa7ZB53DwfFCltZBtIUuouhmuTr5pZAOGTC2n404UbHl3tMs1IRFtPGcnOgMcVDsi61kvXZAqJ9fNibmpeg6NxjZA2DrLyS7nWpmAqnVeajHJ5jdpiEwZDZD'
+const token = 'key'
 app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
